f8f511e1dce6486f0555bd31bf348c56fbb33af1
H-Subin <hsubin32@gmail.com>
2021-04-07 15:12:55 +0900

    added button

--- app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
+++ app/src/main/java/com/example/locproviderlist/LocProviderListActivity.java
@@ -2,16 +2,21 @@ package com.example.locproviderlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.List;
 
-public class LocProviderListActivity extends AppCompatActivity {
+public class LocProviderListActivity extends AppCompatActivity implements View.OnClickListener{
     TextView mTextView;
     LocationManager locMgr;
     List<String> locProviders;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +24,16 @@ public class LocProviderListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = findViewById(R.id.textOutput);
+        mButton = findViewById(R.id.button);
         locMgr = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         locProviders = locMgr.getAllProviders();
+        mButton.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
         String s = "";
         for(int i =0; i< locProviders.size(); i++){
             s += "Loc. Provider: " + locProviders.get(i) + "\n" + "Status: " + locMgr.isProviderEnabled(locProviders.get(i)) + "\n\n";
